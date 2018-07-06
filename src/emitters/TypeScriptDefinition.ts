5a63bb4be2c9a3da0e0dd3daeb5adfe72d60c53d
aspirisen <aspirisen@gmail.com>
2018-07-06 18:56:45 +0300

    Fix enums' output

--- src/emitters/TypeScriptDefinition.ts
+++ src/emitters/TypeScriptDefinition.ts
@@ -60,13 +60,14 @@ export class TypeScriptDefinition<
         this.addSpace(lines);
         utils.fillMultilineComment(lines, enumeration);
 
-        lines.push(`export const enum ${enumeration.name}Enum {`);
+        lines.push(`export enum ${enumeration.name}Enum {`);
         _.forEach(enumeration.values, (v, k) => lines.push(`${k} = "${v}",`));
         lines.push(`}`);
 
         this.addSpace(lines);
-        const enumValues = _.map(enumeration.values, (v) => `"${v}"`).join("|");
-        lines.push(`export type ${enumeration.name} = ${enumValues};`);
+        const enumValues = _.map(enumeration.values, (v) => `"${v}"`);
+        const enumType = enumValues.length ? enumValues.join("|") : "string";
+        lines.push(`export type ${enumeration.name} = ${enumType};`);
 
         this.addSpace(lines);
     }
